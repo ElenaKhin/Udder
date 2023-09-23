1974c93007faec0fe58fc0d761129b79f79cf5f5
ElenaKhin <phyuthandarkhin22@gmail.com>
2023-09-23 23:33:04 +0700

    Add Item Model

---
+++ Udder/ItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  ItemModel.swift
+//  Udder
+//
+//  Created by Phyu Thandar Khin on 23/9/2566 BE.
+//
+
+import Foundation
+
+struct ItemModel{
+    var id: String
+    var title: String
+    var detail: String
+}
